901db62435b99baac3a5b12fc8f63c45385489cf
Pedro <spyxtreme41@gmail.com>
2022-12-22 02:01:02 -0300

    Adicionar util.h (header com utilidades genéricas)

---
+++ util.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef __UTIL_h__
+#define __UTIL_h__
+
+// Inclusoes
+#include <stdio.h>
+#include <string.h>
+
+// Funcoes
+/* ---------- UTILIDADES ------------------------------ */
+void util_imprimeTracinhos(int nTracinhos) {
+
+	int i;
+
+	for (i = 0; i < nTracinhos; i++) {
+		printf("-");
+	}
+}
+
+void util_removeQuebraLinhaFinal(char dados[]) {
+	int tamanho;
+	tamanho = strlen(dados);
+	if ((tamanho > 0) && (dados[tamanho - 1] == '\n')) {
+		dados[tamanho - 1] = '\0';
+	}
+}
+
+#endif
