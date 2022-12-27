556d5a403a448d54c66da16e6a0f76569374732a
Pedro <spyxtreme41@gmail.com>
2022-12-27 00:51:30 -0300

    Separação em arquivos.h, membros int* da struct treino_t -> exercicio_t*

--- main.c
+++ main.c
@@ -19,149 +19,14 @@
 #include <locale.h>
 #include <time.h>
 #include "util.h"
+#include "main.h"
+#include "arquivos.h"
 
 // Constantes
-#define SUCESSO				(0)
-#define TAMANHO_NOME		(50+1)
-#define TAMANHO_ENDERECO	(100+1)
-#define TAMANHO_EMAIL		(50+1)
-#define TAMANHO_ALONGAR		(50+1)
-#define NUM_TREINOS_MAX		(26)
-
 
 // Tipos
-typedef struct exercicio_estrutura {
-	char nomeMaquina[TAMANHO_NOME];	// "Supino maquina"
-	unsigned series;				// 3
-	unsigned reps;					// 15
-	float carga_kg;					// Carga em kg (inicializado 0.00kg para ser indefinido)
-} exercicio_t;
-
-typedef struct treino_estrutura {
-	// Criado para possibilitar adicionar várias práticas já cadastradas ao treino
-	unsigned* ExerPorIDs;			// IDs das práticas, sem estrutura* ou ** para fins de gravação em arquivo
-	unsigned nExer;					// número de práticas para viabilizar a alocação dinâmica
-	char alongar[TAMANHO_ALONGAR];	// ex. panturrilhas, lombar
-	// O treino desejado é referenciado por um char identificador = 65 (A)...
-	
-	/* time.h
-	void inicio;
-	void termino;
-	void reaval;
-	*/
-} treino_t;
-
-typedef struct usuario_estrutura {
-	unsigned matricula;					// Matrícula do usuário, até 4294967295
-	char nome[TAMANHO_NOME];			// Nome completo do usuário
-	int TreinosPorIDs[NUM_TREINOS_MAX];	// Ponteiro para a struct treinos
-	// sem variável auxiliar nTreinos, irá de 0 até 25 (A a Z) conferindo if(usuario[i]) antes de executar a iteração
-} usuario_t;
-
-typedef struct academia_estrutura {
-	char nome[TAMANHO_NOME];		// Nome da academia
-	unsigned long long CNPJ;		// CNPJ
-	char endereco[TAMANHO_ENDERECO];// Endereço da academia
-	char email[TAMANHO_EMAIL];		// Email de atendimento ao cliente da academia
-	unsigned long long telefone;	// Telefone de atendimento ao cliente da academia
-	// Struct com o intuito de ser variavel e não vetor, todos os membros inicializados com 0 ou \0 caso o usuário não queira registrá-los
-} academia_t;
 
 // Funções
-void criarNomeArquivo(academia_t* academia, char nome[]) {
-	char str1[sizeof(unsigned long long) + 1];
-	char str2[] = ".txt";
-
-	util_converteInteiroParaTexto((int)academia->CNPJ, str1);
-	strcpy(nome, str1);
-	strncat(nome, str2, 4);
-}
-
-uint8_t carregaExercicios(exercicio_t** exercicios, FILE* arquivo, long* cursor) {
-	return 1;
-	
-	return 0;
-}
-
-uint8_t carregaTreinos(exercicio_t** exercicios, treino_t** treinos, FILE* arquivo, long* cursor) {
-	// Treino A, B, C... Z
-	return carregaExercicios(exercicios, arquivo, cursor) + 1;
-	
-	return false;
-}
-
-uint8_t carregaUsuarios(exercicio_t** exercicios, treino_t** treinos, usuario_t** usuarios, int* nMatriculas, FILE* arquivo, long* cursor) {
-	
-	while (!feof(arquivo)) {
-		fseek(arquivo, 0, SEEK_SET);
-		fscanf(arquivo, "%i", nMatriculas);
-		*cursor = ftell(arquivo);
-		return carregaTreinos(exercicios, treinos, arquivo, cursor) + 1;
-		
-	}
-
-	return false;
-}
-
-uint8_t carregaAcad(exercicio_t** exercicios, treino_t** treinos, usuario_t** usuarios, academia_t* academia, int* nMatriculas) {
-	FILE* arquivo;
-	long cursor = 0;
-	char nome[TAMANHO_NOME];
-	
-	uint8_t status_carregamento = 0;
-
-	arquivo = fopen("db.bin", "rb");
-
-	if (arquivo) {
-		status_carregamento++;
-		fseek(arquivo, 0, SEEK_SET);
-
-		// loop mantido apenas para evitar ler um arquivo vazio
-		while (!feof(arquivo)) {
-			fread(&academia->nome, sizeof(char), TAMANHO_NOME, arquivo);
-			printf("Academia: %s...\n", academia->nome);
-			fread(&academia->CNPJ, sizeof(unsigned long long), 1, arquivo);
-			fread(&academia->endereco, sizeof(char), TAMANHO_ENDERECO, arquivo);
-			fread(&academia->email, sizeof(char), TAMANHO_EMAIL, arquivo);
-			fread(&academia->telefone, sizeof(unsigned long long), 1, arquivo);
-			break;
-		}	
-
-		fclose(arquivo);
-		
-		// academia.txt
-		criarNomeArquivo(academia, nome);
-		arquivo = fopen(nome, "r");
-
-		if (arquivo) {
-			status_carregamento += carregaUsuarios(exercicios, treinos, usuarios, nMatriculas, arquivo, &cursor);
-			
-			switch (status_carregamento) {
-			case 0:
-				break;
-			case 1:
-				usuarios_section:
-				printf("<Usuarios encontrados!>\n");
-				break;
-			case 2:
-				treinos_section:
-				printf("<Treinos encontrados!>\n");
-				goto usuarios_section;
-				break;
-			case 3:
-				printf("<Exercicios encontrados!>\n");
-				goto treinos_section;
-				break;
-			default:
-				fprintf(stderr, "<Erro desconhecido.>\n");
-				break;
-			}
-
-			fclose(arquivo);
-		}
-	}
-	return status_carregamento;
-}
 
 void imprimeUsuarioUnico(usuario_t* usuarioAtual) {
 	printf("%u: %s", usuarioAtual->matricula, usuarioAtual->nome);
@@ -211,25 +76,6 @@ void cadastrarUsuarios(usuario_t* usuarioAtual, treino_t* treinos){
 	default:
 		break;
 	}
-}
-
-void salvaTudo(exercicio_t* exercicios, treino_t* treinos, usuario_t* usuarios, academia_t academia) {
-	FILE* arquivo = fopen("db.bin", "wb");
-	char nome[TAMANHO_NOME];
-
-	if (arquivo) {
-		fseek(arquivo, 0, SEEK_SET);
-
-		fwrite(&academia.nome, sizeof(char), TAMANHO_NOME, arquivo);
-		fwrite(&academia.CNPJ, sizeof(unsigned long long), 1, arquivo);
-		fwrite(&academia.endereco, sizeof(char), TAMANHO_ENDERECO, arquivo);
-		fwrite(&academia.email, sizeof(char), TAMANHO_EMAIL, arquivo);
-		fwrite(&academia.telefone, sizeof(unsigned long long), 1, arquivo);
-
-		fclose(arquivo);
-	}
-	criarNomeArquivo(&academia, nome);
-	arquivo = fopen(nome, "w");
 }
 
 int main(int argc, char** argv) {
